7828d0f00c093041076c487580e983ceae607267
ADDI01 <115276994+ADDI01@users.noreply.github.com>
2023-10-31 01:17:04 +0100

    game.h

--- SPACE_/SPACE_INVADERS/Game.cpp
+++ SPACE_/SPACE_INVADERS/Game.cpp
@@ -5,7 +5,7 @@ Game::Game() {
 	texturePath[CANNONTEXTURE] = "../Images/spaceship.png";
 	texturePath[ALIENSMAPTEXTURE] = "../Images/aliens.png";
 	texturePath[BUNKERTEXTURE] = "../Images/bunker.png";
-	texturePath[STARSTEXTURE] = "../Images/stars.png";
+	texturePath[STARTEXTURE] = "../Images/stars.png";
 
 	// We now create the textures
 	for (uint i = 0; i < NUM_TEXTURES; i++) {
@@ -16,11 +16,11 @@ Game::Game() {
 	// We finally create the game objects
 	cannon = new Cannon(objectProps.BALL_WIDTH, objectProps.BALL_HEIGHT, objectProps.BALL_INIT_POS,
 		Vector2D(0, 0), this, renderer, textures[BALLTEXTURE]);
-	aliensmap = new AliensMap(objectProps.PADDLE_WIDTH, objectProps.PADDLE_HEIGHT,
+	aliensMap = new AliensMap(objectProps.PADDLE_WIDTH, objectProps.PADDLE_HEIGHT,
 		objectProps.PADDLE_INIT_POS, Vector2D(0, 0), Vector2D(ZERO, ZERO), renderer, this, textures[PADDLETEXTURE]);
 	bunker = new Bunker(objectProps.LATERAL_WALL_WIDTH, objectProps.LATERAL_WALL_HEIGHT,
 		objectProps.LATERAL_LEFT_WALL_INIT_POS, renderer, textures[LATERALWALLTEXTURE]);
-	starts = new Stars(objectProps.LATERAL_WALL_WIDTH, objectProps.LATERAL_WALL_HEIGHT,
+	star = new Star(objectProps.LATERAL_WALL_WIDTH, objectProps.LATERAL_WALL_HEIGHT,
 		objectProps.LATERAL_RIGHT_WALL_INIT_POS, renderer, textures[LATERALWALLTEXTURE]);
 }
 
--- SPACE_/SPACE_INVADERS/Game.h
+++ SPACE_/SPACE_INVADERS/Game.h
@@ -1,6 +1,49 @@
-#pragma once
+#ifndef GAME_H_
+#define GAME_H_
+
+#include <SDL.h>
+#include <list>
+#include "Vector2D.h"
+#include "AliensMap.h"
+#include "Bunker.h"
+#include "Laser.h"
+#include "Cannon.h"
+#include "Star.h"
+#include "Texture.h"
+#include "checkML.h"
+#include <exception>
+#include <iostream>
+#include <string>  
+
+using namespace std;
+
+using uint = unsigned int;
+
+const uint WIN_WIDTH = 800;
+const uint WIN_HEIGHT = 600;
+const uint NUM_TEXTURES = 5;
+const int FRAME_RATE = 5;
+
+enum TextureName { CANNONTEXTURE, ALIENSMAPTEXTURE, BUNKERTEXTURE, STARTEXTURE};
+
 class Game
 {
+private:
+	//Punteros a ventana y render
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	//Puntero por cada objeto del juego
+	Cannon* cannon = nullptr;
+	AliensMap* aliensMap = nullptr;
+	Bunker* bunker = nullptr;
+	Star* star = nullptr;
 
+	Texture* textures[NUM_TEXTURES];
+	const char* texturePath[NUM_TEXTURES];
+
+public:
+	Game() { init(); };
+	//~Game();
+	void init();
 };
 
