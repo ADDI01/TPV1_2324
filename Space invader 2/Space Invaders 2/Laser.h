87fcbafa2d4943fd202e9a213dfa6b38ea8c444a
ADDI01 <115276994+ADDI01@users.noreply.github.com>
2023-11-27 23:15:31 +0100

    imagen UFO + nuevos mapas y clase Laser empezada

---
+++ Space invader 2/Space Invaders 2/Laser.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Vector2D.h"
+#include "checkML.h"
+#include "Texture.h"
+
+class Game;
+
+using uint = unsigned int;
+
+enum Father { ALIEN, PLAYER };
+
+class Laser
+{
+private:
+	//Laser's direction
+	static Vector2D<float> _dir;
+	//Pointer to Game class
+	Game* _myGame = nullptr;
+	//Game's renderer
+	SDL_Renderer* _myRenderer = nullptr;
+	//Destination and size of the laser
+	SDL_Rect* _myRect = nullptr;
+	//velocidad de la bala
+	Vector2D<float> _velocity;
+	//Defines the father. 
+	Father _father;
+
+	
+
+public:
+	Laser(Point2D<float> pos, Vector2D<float> velocity, uint father, Game* game, SDL_Renderer* myRenderer,
+		uint w, uint h);
+	~Laser();
+
+	//Game states
+	void render() const;
+	bool update();
+
+	//Specific actions
+	void hit();
+
+	//Getters
+	bool getFather() const { return _father; };
+	SDL_Rect* getRect() const { return _myRect; };
+};
+
+#endif
+
