592ea33d53e99b9cee219e046fbc01cf2744bba5
VerXxuxZ <juserr04@ucm.es>
2023-12-16 23:13:03 +0100

    me quiero matar pipipi

--- Space invaders 3/bin/GameObject.h
+++ Space invaders 3/bin/GameObject.h
@@ -13,7 +13,7 @@ protected:
 	GameState* _game;
 	GameList < GameObject, true>::anchor _it;
 
-protected:
+public:
 	GameObject(GameState* game): _game(game) {};
 
 	//métodos virtuales puros para renderizar, actualizar y guardar los objetos
@@ -21,7 +21,6 @@ protected:
 	virtual void render() const = 0;
 	virtual void update() = 0;
 	virtual void save(std::ostream& out) const = 0;
-public:
 	virtual ~GameObject() {};
 	void setListAnchor(GameList < GameObject, true>::anchor it) { _it = it; };
 };
